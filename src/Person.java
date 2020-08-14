c1c8735a697a3bd5b5a1ff8b5f9a72d84e282d60
Donald Spini <donald.spini68@gmail.com>
2020-08-14 13:53:57 -0500

    OOP

--- src/Person.java
+++ src/Person.java
@@ -53,6 +53,9 @@ public class Person {
         System.out.println(person1.getName().equals(person2.getName()));
         System.out.println(person1 == person2);
 
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
 
         person1 = new Person("Dean");
         person2 = person1;
