02a333bf2fc419b221e19e938689a851d89e16bc
Donald Spini <donald.spini68@gmail.com>
2020-08-17 09:07:21 -0500

    FizzBuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,11 +2,11 @@ import java.util.stream.IntStream;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        fizzBuzzBeforeJava8(100);
-        fizzBuzzInJava8(100);
+        fizzBuzz(100);
+        fizzBuzz(100);
     }
 
-    private static void fizzBuzzBeforeJava8(int num) {
+    private static void fizzBuzz(int num) {
         for (int i = 1; i <= num; i++) {
             if (((i % 5) == 0) && ((i % 7) == 0))
                 System.out.println("fizzbuzz");
@@ -19,7 +19,7 @@ public class FizzBuzz {
         }
     }
 
-    private static void fizzBuzzInJava8(int num) {
+    private static void fizzBuzz(int num) {
         IntStream.rangeClosed(1, 100)
                 .mapToObj(i -> i % 5 == 0 ? (i % 7 == 0 ? "FizzBuzz" : "Fizz") : (i % 7 == 0 ? "Buzz" : i))
                 .forEach(System.out::println);
