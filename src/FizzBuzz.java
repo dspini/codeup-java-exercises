e24b73568f99760a3b4b18ecc271cf64dcc3b74b
Donald Spini <donald.spini68@gmail.com>
2020-08-17 09:13:54 -0500

    FizzBuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -8,11 +8,11 @@ public class FizzBuzz {
 
     private static void fizzBuzz(int num) {
         for (int i = 1; i <= num; i++) {
-            if (((i % 5) == 0) && ((i % 7) == 0))
+            if (((i % 5) == 0) && ((i % 3) == 0))
                 System.out.println("fizzbuzz");
             else if ((i % 5) == 0)
                 System.out.println("fizz");
-            else if ((i % 7) == 0)
+            else if ((i % 3) == 0)
                 System.out.println("buzz");
             else
                 System.out.println(i);
